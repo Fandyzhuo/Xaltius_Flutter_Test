a4f2f0b5f6edb0d5af4eceaef4f859e60a87220f
Fandyzhuo <fandy@alumni.pcr.ac.id>
2021-06-07 21:46:53 +0700

    Finishing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -8,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -28,32 +31,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List data;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<http.Response> fetchData() {
+    return http.get(Uri.parse('http://localhost:8084/data/data'));
+  }
+
+  void getData() async {
+    data = json.decode((await fetchData()).body);
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        child: Center(
+          child: data != null
+              ? ListView.builder(
+                  padding: const EdgeInsets.symmetric(horizontal: 240),
+                  itemCount: data.length + 1 ?? 0,
+                  itemBuilder: (context, i) {
+                    TextStyle textStyle = TextStyle(fontWeight: i == 0 ? FontWeight.bold : FontWeight.normal, color: Colors.white, fontSize: 24);
+                    return Container(
+                      decoration: BoxDecoration(border: Border.all(color: Color(0xff40454B), width: 2)),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: i % 2 == 0 ? Color(0xff353A40) : Color(0xff3E4349),
+                                border: Border(
+                                  right: BorderSide(color: Color(0xff40454B), width: 2),
+                                ),
+                              ),
+                              child: Text(i == 0 ? "Name" : data[i - 1]['name'], style: textStyle),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                color: i % 2 == 0 ? Color(0xff353A40) : Color(0xff3E4349),
+                                border: Border(
+                                  left: BorderSide(color: Color(0xff40454B), width: 2),
+                                ),
+                              ),
+                              child: Text(i == 0 ? "Role" : data[i - 1]['role'], style: textStyle),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                )
+              : CircularProgressIndicator(),
         ),
       ),
-      
     );
   }
 }
